91d657142dff499d3d0bc8b707d8f4a18fc67cf6
SOFTSERVE\sfili <sfili@softserveinc.com>
2018-04-04 16:51:28 +0300

    Create some UI updates

--- client/src/app/components/navbar/navbar.ts
+++ client/src/app/components/navbar/navbar.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'navbar',
     templateUrl: './navbar.html',
     styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class Navbar implements OnInit {
+    username;
+    email;
+    avatar;
+    refresh: Subject<any> = new Subject();
 
     constructor(private authService: AuthService, private router: Router) { }
 
-    onLogoutClick(){
+    ngOnInit() {
+        this.authService.getProfile().subscribe(profile => {
+            this.username = profile.user.username;
+            this.email = profile.user.email;
+            this.avatar = profile.user.avatar;
+        });
+    }
+
+
+    onLogoutClick() {
         this.authService.logout();
         this.router.navigate(['/']);
     }
-                    
+
 }
