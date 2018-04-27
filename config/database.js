b81a685bc1ee9efbb80406912e6847cf4b5ac098
SOFTSERVE\sfili <sfili@softserveinc.com>
2018-04-27 08:54:39 +0300

    resolve conflicts

--- config/database.js
+++ config/database.js
@@ -1,15 +1,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-<<<<<<< HEAD
-    // uri: 'mongodb://localhost:27017/mean-angular-5',
-    uri: 'mongodb://admin:test123@ds259499.mlab.com:59499/memento-app',
-    secret: crypto,
-    db: 'memento-app'
-=======
   // uri: 'mongodb://admin:test123@ds259499.mlab.com:59499/memento-app',
   uri: 'mongodb://localhost:27017/mean-angular-5',
   secret: crypto,
   db: 'mean-angular-5'
->>>>>>> move-to-mlab
 }
